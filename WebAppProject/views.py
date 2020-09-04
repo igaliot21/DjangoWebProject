8d9aabc0824edac0a6b065e09d0d09584c6d492a
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-09-04 11:25:13 +0200

    Proyecto web completo II

--- WebAppProject/views.py
+++ WebAppProject/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("Home")
+
+
+def servicios(request):
+    return HttpResponse("Servicios")
+
+
+def tienda(request):
+    return HttpResponse("Tienda")
+
+
+def blog(request):
+    return HttpResponse("Blog")
+
+
+def contacto(request):
+    return HttpResponse("Contacto")
