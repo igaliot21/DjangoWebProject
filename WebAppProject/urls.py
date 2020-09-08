765cc6ebda88f098144786d8c406bf6a9f51c91e
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-09-08 11:13:44 +0200

    Proyecto web completo VI

--- WebAppProject/urls.py
+++ WebAppProject/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.home, name="Home"),
     path('servicios/', views.servicios, name="Servicios"),
     path('tienda/', views.tienda, name="Tienda"),
-    path('blog/', views.blog, name="Blog"),
     path('contacto/', views.contacto, name="Contacto"),
+    path('blog/', views.blog, name="Blog"),
 ]
