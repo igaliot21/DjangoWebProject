6ab1077fb8e17ad259243218031ff5a741e31a5c
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-09-09 14:53:14 +0200

    Proyecto web completo VIII

--- WebAppServicios/admin.py
+++ WebAppServicios/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from WebAppServicios.models import servicio
 
 # Register your models here.
+
+
+class ServiciosAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+    list_display = ("titulo", "contenido", "imagen", "created", "updated")
+    search_fields = ("titulo", "created", "updated")
+    date_hierarchy = "updated"
+
+
+admin.site.register(servicio, ServiciosAdmin)
