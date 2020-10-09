dce0a2b8c24b6d3ee8c3d91699cea25b4eb77e3b
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-10-09 09:02:29 +0200

    Proyecto web completo XVI

--- WebAppBlog/urls.py
+++ WebAppBlog/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.blog, name="Blog"),
+    path('Categoria/<int:categoria_id>/', views.categoria, name="Categoria"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- WebAppBlog/views.py
+++ WebAppBlog/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.conf import settings
 from django.template import Template
-from WebAppBlog.models import categoria, post
+from WebAppBlog.models import post
+from WebAppBlog.models import categoria
 import datetime
 
 # Create your views here.
@@ -12,3 +13,9 @@ import datetime
 def blog(request):
     posts = post.objects.all()
     return render(request, "WebAppBlog/Blog.html", {"posts": posts})
+
+
+def categoria(request, categoria_id):
+    #categoria_filter = categoria.objects.get(id=categoria_id)
+    posts = post.objects.filter(categorias=categoria_id)
+    return render(request, "WebAppBlog/Categoria.html", {"categoria": categoria_id, "posts": posts})
